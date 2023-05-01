1287ad7b073496ee47cb354431c4abb89baffa8f
Emil Szymczyk <106548124+SzEmil@users.noreply.github.com>
2023-05-01 13:37:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
-import { persistor, store } from 'redux/store';
+import { persistor, store } from 'redux/store'; // to jest ze store a nie ze bilbioteki npm
 import { PersistGate } from 'redux-persist/integration/react';
 import {BrowserRouter } from 'react-router-dom';
 
